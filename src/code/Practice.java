1a41d309e74d69c4135ed426f85f8824171bcc10
raju <rajubailappa5456@gmail.com>
2026-04-08 18:14:58 +0530

    through git bash

--- src/code/Practice.java
+++ src/code/Practice.java
@@ -2,8 +2,16 @@ package code;
 
 public class Practice {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println("Raju is great!");
+		
+		int numbers[] = {1, 2, 3, 4, 5};
+		
+		for(int num : numbers) {
+			System.out.println(num);
+			
+			Thread.sleep(1000);
+		}
 
 	}
 
